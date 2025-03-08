872614953025e788475285b407747d25872e49e8
hm-qa <rtk.it.hm@gmail.com>
2025-03-09 00:48:38 +0300

    Added getMessage() method

--- src/main/java/hm/it/rtk/Main.java
+++ src/main/java/hm/it/rtk/Main.java
@@ -4,9 +4,11 @@ package hm.it.rtk;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello, you're guru, and welcome!");
+        System.out.printf(getMessage());
+        System.out.println();
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -14,4 +16,11 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
+    private static String getMessage() {
+        return ("Hello, qa.guru!");
+    }
 }
+
+
+
